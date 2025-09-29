1f5d76001a5491df435e33448164856fdd8911bb
Daremon <darekadamski14@wp.pl>
2025-09-29 22:25:02 +0200

    feat(api): implementacja middleware rate limiting (#33)

---
+++ api/v1/ping.php
@@ -0,0 +1,10 @@
+﻿<?php
+declare(strict_types=1);
+
+require_once __DIR__.'/bootstrap.php';
+require_once __DIR__.'/rate_limiter.php';
+
+json_response(200, [
+    'pong' => true,
+    'time' => date('c')
+]);
